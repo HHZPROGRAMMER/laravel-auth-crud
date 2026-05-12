c441d90dce46638794fadd7d8eea7530be3df5fe
HHZPROGRAMMER <hasanhafizov258@gmail.com>
2026-05-12 23:19:32 +0500

    bazi ozgarishlar

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -6,5 +6,32 @@ use Illuminate\Http\Request;
 
 class CrudController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('index');
+    }
+
+    public function create(){
+
+    }
+
+    public function store(Request $request){
+
+    }
+
+    public function show($id){
+
+    }
+
+    public function edit($id){
+
+    }
+
+    public function update(Request $request, $id){
+
+    }
+
+    public function destroy($id){
+
+    }
 }
---
+++ resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Malumotlar</title>
+</head>
+<body>
+    
+
+    <table border="1" cellspacing="0" cellpadding="10">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>name</th>
+                <th>language</th>
+                <th>phonenumber</th>
+                <th>email</th>
+                <th>Edit, show, Delete buttuns</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Hasan</td>
+                <td>Uzbek</td>
+                <td>+998916487373</td>
+                <td>hasanhafizov258@gmail.com</td>
+                <td>
+                    <button>Edit</button>
+                    <button>Show</button>
+                    <button>Delete</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+
+
+</body>
+</html>
