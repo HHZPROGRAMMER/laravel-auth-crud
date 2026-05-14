02e1860afec1aac62ce754150b005f37324b81c1
HHZPROGRAMMER <hasanhafizov258@gmail.com>
2026-05-14 13:09:03 +0500

    git add .!

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,22 @@
 <?php
 
 use App\Http\Controllers\CrudController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [CrudController::class, 'index'])->name('index');
+Route::get('/', [LoginController::class, 'login'])->name('login');
+Route::post('/login', [LoginController::class, 'loginForm'])->name('loginForm');
+
+Route::get('/register', [RegisterController::class, 'register'])->name('register');
+Route::post('/registerForm', [RegisterController::class, 'registerForm'])->name('registerForm');
+
+Route::get('/index', [CrudController::class, 'index'])->name('index');
 Route::get('/create', [CrudController::class, 'create'])->name('create');
 Route::post('/store', [CrudController::class, 'store'])->name('store');
 Route::get('/show/{id}', [CrudController::class, 'show'])->name('show');
 Route::get('/edit/{id}', [CrudController::class, 'edit'])->name('edit');
 Route::post('/update/{id}', [CrudController::class, 'update'])->name('update');
 Route::delete('/destroy/{id}', [CrudController::class, 'destroy'])->name('destroy');
+
+
